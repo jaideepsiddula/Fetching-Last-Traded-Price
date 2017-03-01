96a9efb94a4f2437b8773c09261662875aca0f96
jaideepsiddula <jaideepsiddula@gmail.com>
2017-03-01 19:23:08 +0530

    Create LastTradedPrice.py

---
+++ LastTradedPrice.py
@@ -0,0 +1,13 @@
+from googlefinance import getQuotes
+import json
+def getdata(security):
+	while True:
+		try:
+			price=json.dumps(getQuotes(security))
+			data=json.loads(price)
+			price=data[0]['LastTradePrice']
+			return float(price)
+		except:
+			continue
+data=getdata("NASDAQ:AAPL")
+print(data)
