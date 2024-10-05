6c7829abfe263c236e2d8fb3e169fc3421d3a3e3
jaideepsiddula <jaideepsiddula2@gmail.com>
2024-10-05 21:11:24 +0530

    revised

--- LTP.py
+++ LTP.py
@@ -3,11 +3,11 @@ import json
 def getdata(security):
 	while True:
 		try:
-			price=json.dumps(getQuotes(security))
-			data=json.loads(price)
-			price=data[0]['LastTradePrice']
+			price = json.dumps(getQuotes(security))
+			data = json.loads(price)
+			price = data[0]['LastTradePrice']
 			return float(price)
 		except:
 			continue
-data=getdata("NASDAQ:AAPL")
+data = getdata("NASDAQ:AAPL")
 print(data)
